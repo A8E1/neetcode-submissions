99b13e3503632e31466cf0c6040c211626dfb1b4
A8E1 <98847922+A8E1@users.noreply.github.com>
2026-07-28 11:00:36 -0500

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1_count = {}
+        for letter in s1:
+            s1_count[letter] = s1_count.get(letter, 0) + 1
+        
+        l = 0
+        win_count = {}
+        for r in range(len(s2)):
+            win_count[s2[r]] = win_count.get(s2[r], 0) + 1
+
+            if (r - l + 1) > len(s1):
+                win_count[s2[l]] -= 1
+                if win_count[s2[l]] == 0:
+                    win_count.pop(s2[l])
+                l+=1
+            
+            if win_count == s1_count:
+                return True
+
+        return False
