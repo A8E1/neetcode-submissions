d0495a37319189a16cef15bf13b914c61198bafb
A8E1 <98847922+A8E1@users.noreply.github.com>
2026-08-06 19:41:09 -0500

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.py
@@ -0,0 +1,31 @@
+class TimeMap:
+
+    def __init__(self):
+        self.time_map = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.time_map[key].append([timestamp, value])
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        length_of_value_list = len(self.time_map.get(key, ""))
+        if length_of_value_list == 0:
+            return ""
+        
+        l, r = 0, length_of_value_list-1
+        max_timestamp_val = [-1, ""]
+        while l <= r:
+            mid = (l + r) // 2
+            mid_timestamp = self.time_map[key][mid][0]
+            if mid_timestamp > timestamp:
+                r = mid - 1
+            else:
+                curr_rec_timestamp = max_timestamp_val[0]
+                if max(curr_rec_timestamp, mid_timestamp) == mid_timestamp:
+                    max_timestamp_val = [mid_timestamp, self.time_map[key][mid][1]]
+                l = mid+1
+        
+        return max_timestamp_val[1]
+
+                
+
